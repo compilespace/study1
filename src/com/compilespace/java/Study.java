ca480d5ac6e83d5884e67151514621365586821c
compilespace <879598552@qq.com>
2019-02-28 19:28:43 +0800

    Initial commit

---
+++ src/com/compilespace/java/Study.java
@@ -0,0 +1,39 @@
+/**
+ * Copyright (C), 2018-2019, XXX????
+ * FileName: Study
+ * Author:   Admin
+ * Date:     2019/2/28 15:24
+ * Description: ??
+ * History:
+ * <author>          <time>          <version>          <desc>
+ * ????           ????           ???              ??
+ */
+package com.compilespace.java;
+
+import java.util.ArrayList;
+import java.util.List;
+/*????*/
+/**
+ * ?????????<br> 
+ * ????
+ *
+ * @author Admin
+ * @create 2019/2/28
+ * @since 1.0.0
+ */
+public class Study {
+    public static void main(String[] args) {
+
+    }
+
+    /**
+     *
+     * @param num
+     * @return
+     */
+    public static synchronized String fav(int num){
+        List list = new ArrayList<>();
+        return null;
+    }
+}
+
---
+++ web2/web/index.jsp
@@ -0,0 +1,16 @@
+<%--
+  Created by IntelliJ IDEA.
+  User: Admin
+  Date: 2019/2/28
+  Time: 18:03
+  To change this template use File | Settings | File Templates.
+--%>
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<html>
+  <head>
+    <title>我的第一个网页</title>
+  </head>
+  <body>
+        <h1 style="color:mediumvioletred">第一次编写网页</h1>
+  </body>
+</html>
